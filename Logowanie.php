8058c3ddeecac2c0f04f11bc644033cc080bf885
rudyryd <rudek632632@gmail.com>
2024-03-07 23:00:57 +0100

    start

--- Logowanie.php
+++ Logowanie.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta charset="utf-8">
     <title>Strona Główna</title>
@@ -9,8 +10,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
-    
+
     <div class="naglowek">
         <a href="StronaGlowna.html"><img src="logo.png" alt="logo" height="100" class="logo"></a>
         <p class="tytul">Strona Główna</p>
@@ -18,18 +20,16 @@
 
     <div style="overflow:auto">
         <section class="tall">
-    <nav class="jmenu">
-      <label for="menu-btn" class="jm-menu-btn jm-icon-menu"></label>
-      <input type="checkbox" id="menu-btn" class="jm-menu-btn">
-      <ul class="jm-collapse">
-        <li><a href="StronaGlowna.html">Strona Główna</a></li>
-        <li><a href="ONas.html">O Nas</a></li>
-        <li><a href="Galeria.html">Galeria</a></li>
-        <li><a href="Kontakt.html">Kontakt</a></li>
-        <li class="logowanie"><a href="Logowanie.php">Zaloguj</a></li>
-      </ul>
-    </nav>
-  </section>
+            <nav class="jmenu">
+                <label for="menu-btn" class="jm-menu-btn jm-icon-menu"></label>
+                <input type="checkbox" id="menu-btn" class="jm-menu-btn">
+                <ul class="jm-collapse">
+                    <li><a href="StronaGlowna.html">Strona Główna</a></li>
+
+                    <li class="logowanie"><a href="Logowanie.php">Zaloguj</a></li>
+                </ul>
+            </nav>
+        </section>
         <div class="zawartosc">
             <form class="formularz">
                 <label for="email">Email:</label><br>
@@ -41,13 +41,14 @@
         </div>
 
         <div class="prawa">
-            
+
         </div>
     </div>
 
 
     <div class="stopka">Stopka</div>
-    
-    
+
+
 </body>
+
 </html>
